78119c6cf3a7ba92caaa1afb75fb46d96856564f
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-01-03 23:10:54 +0200

    create routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
-import { ReactDOM } from 'react';
-import App from './App'
+import  ReactDOM  from 'react-dom/client';
+import App from './App';
+import './index.css';
 const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);

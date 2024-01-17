e4e571e1a4bf07af46c6319d9001d9a7e751dfd6
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-01-17 14:53:24 +0200

    move sidebar down

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos } from "./";
+import { Videos, Sidebar } from "./";
 
 
 const Feed = () => {
@@ -19,7 +19,8 @@ const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-{/*        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />*/}        
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2024 ViewTube
         </Typography>

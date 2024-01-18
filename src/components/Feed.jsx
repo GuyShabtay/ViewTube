c7f95bd869d6fc3c7b7a234732f129692256341e
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-01-18 02:50:16 +0200

    change import in the feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos } from "./";
-import { Sidebar } from "./Sidebar";
+import { Videos,Sidebar } from "./";
  
 
 const Feed = () => {
